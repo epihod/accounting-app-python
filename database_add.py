ddf6a0102c97d83da94ea03a3471326ba1254d6f
epihod <129109482+epihod@users.noreply.github.com>
2024-06-04 22:05:50 +0330

    Delete database_add.py

--- database_add.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-username = "epihod"
-with sqlite3.connect('project/accounting.db') as conn:
-    cur = conn.cursor()
-    cur.execute("select * from categories")
-    c = cur.fetchall()
-    for i in c:
-        if i[0]== username:
-            print(i[1], end = ", ")
